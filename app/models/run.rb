228920d5556cb3b46b636c77e52876a4b15bb8ae
matthieulpro <matthieu.lu.pro@gmail.com>
2022-03-26 09:50:56 +0100

    Add rspec rubocop and lint existing models

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,10 +1,10 @@
-
 class Run < ApplicationRecord
   # This regex validates format for youtube.com and speedrun.com
-  VIDEO_LINK_REGEX = Constants['video_link_regex']
-  validates: number_death, :length => { minimum: 0, message: t('run.errors.number_death', value: value) }, :allow_nil: true
-  validates :video_link, :format => { 
+  VIDEO_LINK_REGEX = Constants["video_link_regex"]
+  validates :number_death, length: { minimum: 0, message: t("run.errors.number_death", value: value) },
+                           allow_nil: true
+  validates :video_link, format: {
     with: VIDEO_LINK_REGEX,
-    message: t('run.errors.video_link')
+    message: t("run.errors.video_link"),
   }
 end
